f40054b576f7f7c75b6783025fd870483e894991
eavillagran <eavillagran@uc.cl>
2017-08-01 16:06:25 -0400

    crear usuarios

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'app-usuarios',
-  templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.css']
+    selector: 'app-usuarios',
+    templateUrl: './usuarios.component.html',
+    styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent implements OnInit {
+export class UsuariosComponent {
 
-  constructor() { }
+    usuarios: any;
 
-  ngOnInit() {
-  }
+    constructor(private http: Http) {
+        this.recibir_usuarios();      
+    }
+
+    public recibir_usuarios() {
+        let headers = new Headers();
+        headers.append('token', 'fqH6AyiyhQMeqKM8MjMC');
+        headers.append('Content-Type','application/json');
+
+        this.http.get('http://localhost:5000/get_users', {headers: headers})
+            .map(res => res.json())
+            .subscribe(data => {
+                this.usuarios = data;
+                console.log(data);
+            });
+    }
 
 }
