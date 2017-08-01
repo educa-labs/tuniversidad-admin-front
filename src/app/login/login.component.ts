1082afe8d7bb5bf3419e3210a0ff346272c07cd4
eavillagran <eavillagran@uc.cl>
2017-08-01 18:55:05 -0400

    terminar login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+// Importar providers
+import { AuthService } from '../_services/index';
 
 @Component({
     selector: 'app-login',
@@ -8,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-    constructor() { }
+    credenciales: any = {};
+    data_recibida: any;
+
+    constructor(private auth: AuthService, private router: Router) { }
 
     ngOnInit() {
+        /* ngOnInit: se ejecuta cuando se abre esta parte. Se llama a la 
+        funcion del service que borra el usuario actual. */
         console.log('Se abrió el login');
+        // Resetear el login 
+        this.auth.logout();
     }
 
+    login() {
+        /* login: funcion para hacer login. Manda las credenciales a la funcion
+        login del service. */
+        this.auth.login(this.credenciales.email, this.credenciales.password)
+            .subscribe (
+                data => {
+                    console.log('Data recibida', data);
+                    this.data_recibida = data;
+                    this.router.navigate(['/users']);
+                },
+                error => {
+                    console.log('Error recibido', error);
+                    this.data_recibida = error.json()['error'];
+                })
+    }
 }
