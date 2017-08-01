91c1f6a7fa5994060867bc5b6ec02ecf6de14625
eavillagran <eavillagran@uc.cl>
2017-08-01 14:59:10 -0400

    agregar rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
+// Importar componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
+const rutasApp: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        UsuariosComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot(rutasApp)
     ],
     providers: [],
     bootstrap: [AppComponent]
