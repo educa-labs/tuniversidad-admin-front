3b7fbee988be11f8df3848ef35cfef2592f4821f
eavillagran <eavillagran@uc.cl>
2017-08-01 18:54:51 -0400

    declarar modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 // Importar componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
-import { AlertDirective } from './_directives/alert/alert.directive';
+// Importar guards
+import { AuthGuard } from './_guards/index';
+// Importar services
+import { AuthService } from './_services/index';
+// Importar angular material
+import { MdMenuModule, MdIconModule, MdToolbarModule } from '@angular/material';
 
 const rutasApp: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'users', component: UsuariosComponent }
+    { path: 'users', component: UsuariosComponent, canActivate: [AuthGuard] },
+    // Error 404 lleva home
+    { path: '**', redirectTo: '/users' }
 ];
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
-        UsuariosComponent,
-        AlertDirective
+        UsuariosComponent
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         HttpClientModule,
         HttpModule,
+        MdMenuModule, 
+        MdToolbarModule,
+        MdIconModule,
+        BrowserAnimationsModule,
         RouterModule.forRoot(rutasApp)
     ],
-    providers: [],
+    providers: [
+        AuthGuard,
+        AuthService
+    ],
     bootstrap: [AppComponent]
 })
+
 export class AppModule { }
