a1f3cc72537472c0e0e4b8e67f5df369e9261926
ironcadiz <afcadiz@uc.cl>
2018-06-08 16:22:25 -0400

    Added api update new

--- src/pages/news-show/news-show.ts
+++ src/pages/news-show/news-show.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { NewsProvider } from '../../providers/news/news'
 
 
 @Component({
@@ -8,11 +10,65 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class NewsShowPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  newData: any;
+  token:  string;
+  imageToUpload: File = null;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: Storage,
+    public newProvider: NewsProvider) {
+      this.newData = {}
+      // Extraemos token del usuario
+      this.storage.get("user").then((data) => {
+        this.token = data.token
+      })
+      this.loadNew(navParams.get('newId'))
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad NewsShowPage');
+
+  loadNew(newId) {
+    this.newProvider.getNew(newId).then( data => {
+      this.newData = data
+      console.log(this.newData)
+    })
   }
+
+
+  handleImageInput(files: FileList) {
+    this.imageToUpload = files.item(0);
+  }  
+
+  async update(){
+    const payload = { 
+      new: this.newData
+    }
+
+    if (this.imageToUpload) {
+      const data = await this.readUploadedFileAsData(this.imageToUpload)
+      const result =JSON.stringify(data).slice(1,-1)
+      payload['picture'] = result.split('base64,').pop();
+      payload['extension'] = this.imageToUpload.name.split('.').pop()
+    }
+
+    console.log("payload", payload)
+    this.newProvider.updateNew(this.navParams.get('newId'),payload, this.token)
+        .then(data => {
+          console.log('Respuesta al crear', data);
+    })
+  }
+
+  readUploadedFileAsData = (file) => {
+    const reader:FileReader = new FileReader();
+
+    return new Promise((resolve,reject) => {
+      reader.onloadend = (e) => {
+        resolve(reader.result)
+      }
+      reader.readAsDataURL(file);    
+      }
+    )
+  };
 
 }
--- src/providers/news/news.ts
+++ src/providers/news/news.ts
@@ -49,4 +49,43 @@ export class NewsProvider {
       })
     }
 
+  getNew(newId) {
+    let loader = this.loading.create({ content: 'Cargando noticias...' });
+    // Mostrar loader
+    loader.present();
+    return new Promise(resolve => {
+      // Headers para hacer la consulta
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      // Hacer post a la API
+      this.http.get(this.api + '/news/'+ newId)
+          .map(res => res.json())
+          .subscribe(data => {
+            resolve(data);
+            loader.dismiss();
+          })
+      })
+    }
+
+  
+  updateNew(newId,newData,token) {
+    let loader = this.loading.create({ content: 'Actualizando noticia...' });
+    // Mostrar loader
+    loader.present();
+    return new Promise(resolve => {
+      // Headers para hacer la consulta
+      let headers = new Headers();
+
+      headers.append('Authorization', token);
+      headers.append('Content-Type', 'application/json');
+      // Hacer put a la API
+      this.http.put(this.api + '/news/' + newId, JSON.stringify(newData), { headers: headers })
+          .map(res => res.json())
+          .subscribe(data => {
+            resolve(data);
+            loader.dismiss();
+        })
+      })
+    }
+
 }
